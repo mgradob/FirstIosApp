39dc1c5e7261134942295f2138daa63ed7973f85
Miguel Grado <mgradob@gmail.com>
2016-03-26 20:33:12 -0700

    Changed project to Storyboards.

---
+++ FirstApp/FirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstApp
+//
+//  Created by Miguel Grado on 3/26/16.
+//  Copyright © 2016 Miguel Grado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstApp/FirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstApp
+//
+//  Created by Miguel Grado on 3/26/16.
+//  Copyright © 2016 Miguel Grado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
